3a0b79fd2d4ab40177481db35416ee944ec0ce7b
FCP05 <129459494+FCP05@users.noreply.github.com>
2023-04-07 00:59:40 -0500

    newModel

--- model.py
+++ model.py
@@ -5,61 +5,83 @@
 #   PalaciosTinoco, Flavio C.
 #   Mbwavi, Nickson L.
 #   Polanco, Samuel S.
-#File: AI MODEL STRUCTURE
-#      ->Builds model structure.
+#File: AI MODEL STRUCTURE DEFINITION
+#      ->Defines Model structure..
+
+from keras.models import Model
+from keras.layers import Add, Activation, Concatenate, Conv2D 
+from keras.layers import Flatten, Input, GlobalAveragePooling2D, MaxPooling2D
+import keras.backend as K
 
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+    
+def SqueezeNet(inputShape, numOfLabels):
+    """
+    Basic SqueezeNet Structure
+    (No Bypassing, Compression of "1.0", and No Dropout rate)
+    
+    Arguments:
+        inputShape: shape of the input images e.g. (224,224,3).
+        numOfLabels: number of classes.
+    Returns:
+        Model: Keras Model instance stored as "Model".
+    """
+    input_img = Input(shape=inputShape)
 
-#Define the photo input shape(224pi,224pi,RGB).
-input_shape = (224, 224, 3)
+    layer_x = Conv2D(int(96), (7,7), activation='relu', strides=(2,2), padding='same', name='conv1')(input_img)
 
-#Initialize the model.
-model = Sequential()
+    layer_x = MaxPooling2D(pool_size=(3,3), strides=(2,2), name='maxPool1')(layer_x)
+    
+    layer_x = createFireModule(layer_x, int(16), name='fire2')
+    layer_x = createFireModule(layer_x, int(16), name='fire3')
+    layer_x = createFireModule(layer_x, int(32), name='fire4')
+    
+    layer_x = MaxPooling2D(pool_size=(3,3), strides=(2,2), name='maxPool4')(layer_x)
+    
+    layer_x = createFireModule(layer_x, int(32), name='fire5')
+    layer_x = createFireModule(layer_x, int(48), name='fire6')
+    layer_x = createFireModule(layer_x, int(48), name='fire7')
+    layer_x = createFireModule(layer_x, int(64), name='fire8')
+    
+    layer_x = MaxPooling2D(pool_size=(3,3), strides=(2,2), name='maxPool8')(layer_x)
+    
+    layer_x = createFireModule(layer_x, int(64), name='fire9')
+        
+    layer_x = output(layer_x, numOfLabels)
+    
+    return Model(inputs=input_img, outputs=layer_x)
 
-#CONVOLUTION LAYERS are built with the following parameters:
-#(image filters amount in use, kernel dimensions, padding to be used, activation function in use).
-#POOLING LAYERS are built with the following parmeters:
-#(image filter dimensions, stride of the filter).
+def createFireModule(layer_x, nb_squeeze_filter, name):
+    """
+    Creates a fire module
+    
+    Arguments:
+        layer_x: Input
+        nb_squeeze_filter: number of filters of squeeze. The filtersize of elayer_xpand is 4 times of squeeze
+        name: name of module e.g. fire123
+    
+    Returns:
+        layer_x: returns a fire module
+    """
+    
+    nb_expand_filter = 4 * nb_squeeze_filter
+    squeeze    = Conv2D(nb_squeeze_filter,(1,1), activation='relu', padding='same', name='%s_squeeze'%name)(layer_x)
+    expand_1x1 = Conv2D(nb_expand_filter, (1,1), activation='relu', padding='same', name='%s_expand_1x1'%name)(squeeze)
+    expand_3x3 = Conv2D(nb_expand_filter, (3,3), activation='relu', padding='same', name='%s_expand_3x3'%name)(squeeze)
+    
+    alayer_x = getALayer_X()
+    layer_x_ret = Concatenate(axis=alayer_x, name='%s_Concatenate'%name)([expand_1x1, expand_3x3])
+        
+    return layer_x_ret
 
-#Add the first set of convolutional layers.
-model.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu', input_shape=input_shape))
-model.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-#Add the second set of convolutional layers.
-model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-#Add the third set of convolutional layers.
-model.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-#Add the fourth set of convolutional layers.
-model.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-#Add the fifth set of convolutional layers.
-model.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-#FULLY CONNECTERD LAYERS process a "1-D" feature map. These layers are 
-#populated with a number of desired nodes. The final layer designates
-#the number of possible outputs available. 
-
-#Add the fully connected layers.
-model.add(Flatten())
-model.add(Dense(4096, activation='relu'))
-model.add(Dense(4096, activation='relu'))
-model.add(Dense(8, activation='softmax'))
-
-#Compile the model.
-model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+def getALayer_X():
+    alayer_x = -1 if K.image_data_format() == 'channels_last' else 1
+    return alayer_x
+    
+   
+def output(layer_x, numOfLabels):
+    layer_x = Conv2D(numOfLabels, (1,1), strides=(1,1), padding='valid', name='conv10')(layer_x)
+    layer_x = GlobalAveragePooling2D(name='avgPool10')(layer_x)
+    layer_x = Activation("softmax", name='softmax')(layer_x)
+    return layer_x
